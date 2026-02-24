95b663383239a9bc78c042ce710076790a53f485
Jassiel Ovando <jassielovando@protonmail.com>
2026-02-24 11:27:12 -0400

    no cli

--- src/cli/main.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const metadata = @import("../../build.zig.zon");
-const fangz = @import("fangz");
-
-pub fn main() !void {
-    const version = metadata.version;
-    std.debug.print("yaml-zig version: {s}\n", .{version});
-    std.debug.print("Hola\n", .{});
-}
