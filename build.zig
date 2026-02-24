70c716b2c2294745c2f76451e3ae0f6aa8ea2b5a
Jassiel Ovando <jassielovando@protonmail.com>
2026-02-24 10:17:48 -0400

    fangz

--- build.zig
+++ build.zig
@@ -6,6 +6,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const fangz_dep = b.dependency(
+        "fangz",
+        .{ .target = target, .optimize = optimize },
+    );
+
     const lib_mod = b.addModule(mod_name, .{
         .root_source_file = b.path("src/lib/root.zig"),
         .target = target,
@@ -16,6 +21,31 @@ pub fn build(b: *std.Build) void {
         .name = mod_name,
         .root_module = lib_mod,
     });
+
+    const cli = b.addExecutable(.{
+        .name = mod_name ++ "-cli",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/cli//main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{
+                    .name = mod_name,
+                    .module = lib_mod,
+                },
+                .{
+                    .name = "fangz",
+                    .module = fangz_dep.module("fangz"),
+                },
+            },
+        }),
+    });
+
+    b.installArtifact(cli);
+
+    const cli_step = b.step("cli", "Build the CLI");
+    const run_cli = b.addRunArtifact(cli);
+    cli_step.dependOn(&run_cli.step);
 
     const docs = b.addInstallDirectory(.{
         .source_dir = lib.getEmittedDocs(),
