0005478590420b68e2ef43dd5e497eb0c43fc267
Jassiel Ovando <jassielovando@protonmail.com>
2026-05-05 12:10:51 -0400

    hmm

--- src/lib/root.zig
+++ src/lib/root.zig
@@ -1,5 +1,10 @@
 //! YAML.
+const std = @import("std");
+const refAllDecls = std.testing.refAllDecls;
 
 pub const fy = @import("fy");
+pub const yaml = @import("yaml");
 
-pub const yaml = @import("yaml.zig");
+comptime {
+    refAllDecls(@This());
+}
