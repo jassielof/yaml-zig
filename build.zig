929bc113765de3ce5eac311af5faed9749680f7b
Jassiel Ovando <jassielovando@protonmail.com>
2026-09-23 21:41:20 -0400

    ci: update CI configuration for improved code quality checks and consistency

--- build.zig
+++ build.zig
@@ -3,16 +3,13 @@ const std = @import("std");
 const fy = @import("build/fy.zig");
 
 pub fn build(b: *std.Build) void {
-    const mod_name = "yaml";
-    const fy_mod_name = "fy";
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const summary_only = b.option(bool, "summary", "Only print short coverage summary (useful for CI)") orelse false;
 
-    const lib_mod = b.addModule(
-        mod_name,
+    const yaml_mod = b.addModule(
+        "yaml",
         .{
             .root_source_file = b.path("lib/yaml/root.zig"),
             .target = target,
@@ -21,52 +18,48 @@ pub fn build(b: *std.Build) void {
     );
 
     const fy_dep = fy.create(b, .{
-        .module_name = fy_mod_name,
+        .module_name = "fy",
         .target = target,
         .optimize = optimize,
     });
 
     const docs_step = b.step("docs", "Generate the documentation");
-    const docs_lib = b.addLibrary(.{
+    const yaml_lib = b.addLibrary(.{
         .name = "yaml",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("lib/yaml/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = yaml_mod,
     });
-    const docs = b.addInstallDirectory(.{
-        .source_dir = docs_lib.getEmittedDocs(),
+    const yaml_docs = b.addInstallDirectory(.{
+        .source_dir = yaml_lib.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "docs",
     });
-    docs_step.dependOn(&docs.step);
+    docs_step.dependOn(&yaml_docs.step);
 
     const build_options = b.addOptions();
     build_options.addOption(bool, "summary_only", summary_only);
 
     const test_step = b.step("test", "Run the test suite");
 
-    const unit_tests = b.addTest(.{
-        .name = "yaml",
+    const yaml_test = b.addTest(.{
+        .name = "YAML",
         .root_module = b.createModule(.{
             .root_source_file = b.path("lib/yaml/root.zig"),
             .target = target,
             .optimize = optimize,
         }),
     });
-    test_step.dependOn(&b.addRunArtifact(unit_tests).step);
+    test_step.dependOn(&b.addRunArtifact(yaml_test).step);
 
     const spec_tests = b.addTest(.{
-        .name = "spec",
+        .name = "Specification Compliance",
         .root_module = b.createModule(.{
             .root_source_file = b.path("tests/spec.zig"),
             .optimize = optimize,
             .target = target,
             .link_libc = true,
             .imports = &.{
-                .{ .name = mod_name, .module = lib_mod },
-                .{ .name = fy_mod_name, .module = fy_dep.module },
+                .{ .name = "yaml", .module = yaml_mod },
+                .{ .name = "fy", .module = fy_dep.module },
                 .{ .name = "build_options", .module = build_options.createModule() },
             },
         }),
@@ -85,4 +78,10 @@ pub fn build(b: *std.Build) void {
     const run_coverage_summary = b.addRunArtifact(coverage_summary);
     const coverage_summary_step = b.step("coverage-summary", "Render YAML spec coverage into GITHUB_STEP_SUMMARY or stdout");
     coverage_summary_step.dependOn(&run_coverage_summary.step);
+
+    const check_step = b.step("check", "Run code quality checks");
+    const fmt = b.addFmt(.{ .check = true, .paths = &.{
+        "lib",
+    } });
+    check_step.dependOn(&fmt.step);
 }
